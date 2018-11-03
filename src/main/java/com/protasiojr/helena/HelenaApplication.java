2539c96c5f2ce81d7e620b7674b45694813de3a3
Protasiojr <joaoprotasiojr@gmail.com>
2018-11-02 22:38:27 -0400

    inicio do projeto

---
+++ src/main/java/com/protasiojr/helena/HelenaApplication.java
@@ -0,0 +1,12 @@
+package com.protasiojr.helena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelenaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/protasiojr/helena/HelenaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.protasiojr.helena;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelenaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
