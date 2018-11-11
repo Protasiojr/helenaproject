e42ebc6da11c9cf5a205bbb5188027b51edad197
Protasiojr <joaoprotasiojr@gmail.com>
2018-11-10 23:52:50 -0400

    Criando operação de instanciação na classe PRINCIPAL

--- src/main/java/com/protasiojr/helena/HelenaApplication.java
+++ src/main/java/com/protasiojr/helena/HelenaApplication.java
@@ -1,12 +1,31 @@
 package com.protasiojr.helena;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.protasiojr.helena.domain.Categoria;
+import com.protasiojr.helena.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class HelenaApplication {
+public class HelenaApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelenaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+				
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
